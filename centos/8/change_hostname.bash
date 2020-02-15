503de07a814df4ebd2f0a01d3715b5e122b0b0f5
Alan Redford Hayes <alanredfordhayes@gmail.com>
2020-02-15 18:47:28 -0500

    command not found

--- centos/8/change_hostname.bash
+++ centos/8/change_hostname.bash
@@ -2,14 +2,15 @@
 
 
 CURRENT_HOSTNAME=$(hostname)
-echo $CURRENT_HOSTNAME
+echo "Current hostname: $CURRENT_HOSTNAME"
 
 
 if [ -z "$1" ]
 then
 INPUT1="test-server-01"
-echo "No input at commandline. Hence, server will be named the following: $INPUT" 
+echo "No input detected at commandline." 
 else
+echo "Input detected at commandline"
 INPUT1=$1
 fi
 
@@ -17,6 +18,7 @@ fi
 if [ "$CURRENT_HOSTNAME" == "$INPUT1" ]
 then
 echo "Current hostname matches the input value."
+echo "Current hostname: $CURRENT_HOSTNAME"
 else
 echo "Current hostname does not match the input value."
 echo "Current hostname: $CURRENT_HOSTNAME"
