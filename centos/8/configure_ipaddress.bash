ce3d1c81f889e4b37269ec521786d798cdf54129
Alan Redford Hayes <alanredfordhayes@gmail.com>
2020-02-18 12:14:43 -0500

    Network interfaces array

--- centos/8/configure_ipaddress.bash
+++ centos/8/configure_ipaddress.bash
@@ -2,5 +2,11 @@
 
 
 IPADDR=$(ip address | grep "^[0-9]:" | cut -d " " -f 2)
+echo $IPADDR
+
 NETWORK_INTERFACES=$(echo $IPADDR | sed "s/ /,/g")
 echo $NETWORK_INTERFACES
+
+
+IFS=', ' read -r -a NETWORK_INTERFACES_ARRAY <<< "$NETWORK_INTERFACES"
+echo $NETWORK_INTERFACES_ARRAY
